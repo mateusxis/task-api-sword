41a800f27944386bcb6150d0fb753e2e1deba14e
mateusxis <mateusx@poli.ufrj.br>
2023-07-09 22:20:54 +0100

    chore: refactor taskDomain

--- src/domain/task/task.js
+++ src/domain/task/task.js
@@ -1,17 +1,100 @@
 const { attributes } = require('structure');
 
-const Task = attributes({
-  id: Number,
-  summary: {
-    type: String,
-    maxLength: 2500
-  },
-  userId: {
-    type: Number,
-    required: true
-  },
-  executedAt: Date,
-  createdAt: Date
-})(class Task {});
+const taskDomain = ({ logger, taskRepository }) => {
+  const MAP_TASK_GET = {
+    DEV: taskRepository.getByUserId,
+    MANAGER: taskRepository.getAll
+  };
 
-module.exports = Task;
+  const LIST_ROLES_WITH_EXCLUSION = ['MANAGER'];
+
+  const Task = attributes({
+    id: Number,
+    summary: {
+      type: String,
+      maxLength: 2500
+    },
+    userId: {
+      type: Number,
+      required: true
+    },
+    executedAt: Date,
+    createdAt: Date
+  })(class Task {});
+
+  const validate = ({ id, summary, userId, executedAt, createdAt }) => {
+    const task = new Task({ id, summary, userId, executedAt, createdAt });
+    const { valid, errors } = task.validate({ id, summary, userId, executedAt, createdAt });
+
+    return { valid, errors, data: task.toJSON() };
+  };
+
+  const get = async ({ userId, role }) => {
+    const find = MAP_TASK_GET[`${role}`];
+
+    if (!find) {
+      const error = new Error(`Your role ${role} does not exist`);
+      logger.error(error);
+      throw error;
+    }
+
+    const task = await find({ userId });
+
+    return task;
+  };
+
+  const remove = async ({ id, role }) => {
+    if (!LIST_ROLES_WITH_EXCLUSION.includes(role)) {
+      const error = new Error(`You don't have permission to delete task ${id}`);
+      logger.error(error);
+      throw error;
+    }
+
+    const task = await taskRepository.remove({ id });
+
+    return task;
+  };
+
+  const save = async ({ summary, userId }) => {
+    const { valid, errors, data } = validate({ userId, summary });
+    if (!valid) {
+      logger.error(errors);
+      const error = new Error('There are one or more invalid fields');
+      error.errors = errors;
+      throw error;
+    }
+
+    const task = await taskRepository.save({ userId: data.userId, summary: data.summary });
+
+    return task;
+  };
+
+  const update = async ({ summary, executedAt, userId, id }) => {
+    const { valid, errors, data } = validate({ summary, executedAt, userId, id });
+    if (!valid) {
+      logger.error(errors);
+      const error = new Error('There are one or more invalid fields');
+      error.errors = errors;
+      throw error;
+    }
+
+    const task = await taskRepository.update({
+      summary: data.summary,
+      executedAt: data.executedAt,
+      userId: data.userId,
+      id: data.id
+    });
+
+    return task;
+  };
+
+  return {
+    get,
+    remove,
+    save,
+    validate,
+    update
+  };
+};
+
+module.exports = taskDomain;
--- src/domain/task/task.test.js
+++ src/domain/task/task.test.js
@@ -1,36 +1,169 @@
-const validTaskFixture = require('./__fixtures__/validTask.json');
-const largerSummaryTaskFixture = require('./__fixtures__/largerSummaryTask.json');
+const taskDomainFactory = require('./task');
 
-const Task = require('./task');
+const fakeLogger = require('../__mocks__/fakeLogger');
+const fakeTaskRepository = require('../__mocks__/fakeTaskRepository');
+const validTaskFixture = require('../__fixtures__/validTask.json');
+const largerSummaryTaskFixture = require('../__fixtures__/largerSummaryTask.json');
+
+beforeEach(() => {
+  jest.clearAllMocks();
+});
 
 describe('TaskDomain', () => {
-  it('should return data properly', () => {
-    const task = new Task(validTaskFixture);
-    const { valid } = task.validate(validTaskFixture);
+  describe('validate()', () => {
+    const taskDomain = taskDomainFactory({
+      logger: fakeLogger,
+      taskRepository: fakeTaskRepository
+    });
 
-    expect(valid).toBeTruthy();
-    expect(task.toJSON().id).toEqual(validTaskFixture.id);
-    expect(task.toJSON().summary).toEqual(validTaskFixture.summary);
-    expect(task.toJSON().userId).toEqual(validTaskFixture.userId);
-    expect(task.toJSON().createdAt).toEqual(new Date(validTaskFixture.createdAt));
-    expect(task.toJSON().executedAt).toEqual(new Date(validTaskFixture.executedAt));
+    it('should return data properly', () => {
+      const { valid, data } = taskDomain.validate(validTaskFixture);
+
+      expect(valid).toBeTruthy();
+      expect(data.id).toEqual(validTaskFixture.id);
+      expect(data.summary).toEqual(validTaskFixture.summary);
+      expect(data.userId).toEqual(validTaskFixture.userId);
+      expect(data.executedAt).toEqual(new Date(validTaskFixture.executedAt));
+      expect(data.createdAt).toEqual(new Date(validTaskFixture.createdAt));
+    });
+
+    it('should fail with invalid email address', () => {
+      const { valid, errors } = taskDomain.validate(largerSummaryTaskFixture);
+
+      expect(valid).toBeFalsy();
+      expect(Array.isArray(errors)).toBeTruthy();
+      expect(errors[0].message).toEqual('"summary" length must be less than or equal to 2500 characters long');
+    });
+
+    it('should fail when all fields are not provided or empty', () => {
+      const { valid, errors } = taskDomain.validate({});
+
+      expect(valid).toBeFalsy();
+      expect(Array.isArray(errors)).toBeTruthy();
+      expect(errors[0].message).toEqual('"userId" is required');
+    });
   });
 
-  it('should fail with invalid email address', () => {
-    const task = new Task(largerSummaryTaskFixture);
-    const { valid, errors } = task.validate(largerSummaryTaskFixture);
+  describe('get()', () => {
+    const taskDomain = taskDomainFactory({
+      logger: fakeLogger,
+      taskRepository: fakeTaskRepository
+    });
 
-    expect(valid).toBeFalsy();
-    expect(Array.isArray(errors)).toBeTruthy();
-    expect(errors[0].message).toEqual('"summary" length must be less than or equal to 2500 characters long');
+    it('should call getAll then role user is MANAGER', async () => {
+      const validUserFixture = {
+        userId: 1,
+        role: 'MANAGER'
+      };
+      const auth = await taskDomain.get(validUserFixture);
+
+      expect(Array.isArray(auth)).toBeTruthy();
+      expect(fakeTaskRepository.getAll).toHaveBeenCalledWith({ userId: validUserFixture.userId });
+    });
+
+    it('should call getAll then role user is DEV', async () => {
+      const validUserFixture = {
+        userId: 2,
+        role: 'DEV'
+      };
+      const auth = await taskDomain.get(validUserFixture);
+
+      expect(Array.isArray(auth)).toBeTruthy();
+      expect(fakeTaskRepository.getByUserId).toHaveBeenCalledWith({ userId: validUserFixture.userId });
+    });
+
+    it('should call getAll then role user is invalid', async () => {
+      const validUserFixture = {
+        userId: 3,
+        role: 'NONE'
+      };
+
+      await expect(taskDomain.get(validUserFixture)).rejects.toThrowError('Your role NONE does not exist');
+      expect(fakeTaskRepository.getByUserId).not.toHaveBeenCalled();
+      expect(fakeTaskRepository.getByUserId).not.toHaveBeenCalled();
+    });
   });
 
-  it('should fail when all fields are not provided or empty', () => {
-    const task = new Task({});
-    const { valid, errors } = task.validate({});
+  describe('remove()', () => {
+    const taskDomain = taskDomainFactory({
+      logger: fakeLogger,
+      taskRepository: fakeTaskRepository
+    });
 
-    expect(valid).toBeFalsy();
-    expect(Array.isArray(errors)).toBeTruthy();
-    expect(errors[0].message).toEqual('"userId" is required');
+    it('should call remove then role user is MANAGER', async () => {
+      await taskDomain.remove({
+        id: 1,
+        role: 'MANAGER'
+      });
+
+      expect(fakeTaskRepository.remove).toHaveBeenCalledWith({ id: 1 });
+    });
+
+    it('should not call remove then role user is invalid', async () => {
+      await expect(taskDomain.remove({ id: 1, role: 'DEV' })).rejects.toThrowError(
+        `You don't have permission to delete task 1`
+      );
+      expect(fakeTaskRepository.remove).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('save()', () => {
+    const taskDomain = taskDomainFactory({
+      logger: fakeLogger,
+      taskRepository: fakeTaskRepository
+    });
+
+    it('should call repository with task to save', async () => {
+      await taskDomain.save({
+        userId: validTaskFixture.userId,
+        summary: validTaskFixture.summary
+      });
+
+      expect(fakeTaskRepository.save).toHaveBeenCalledWith({
+        userId: validTaskFixture.userId,
+        summary: validTaskFixture.summary
+      });
+    });
+
+    it('should not call repository then userId is undefined', async () => {
+      await expect(taskDomain.save({ summary: validTaskFixture.summary })).rejects.toThrowError(
+        `There are one or more invalid fields`
+      );
+      expect(fakeTaskRepository.save).not.toHaveBeenCalled();
+    });
+  });
+
+  describe('update()', () => {
+    const taskDomain = taskDomainFactory({
+      logger: fakeLogger,
+      taskRepository: fakeTaskRepository
+    });
+
+    it('should call repository with task to update', async () => {
+      const { summary, executedAt, userId, id } = validTaskFixture;
+
+      await taskDomain.update({
+        summary,
+        executedAt,
+        userId,
+        id
+      });
+
+      expect(fakeTaskRepository.update).toHaveBeenCalledWith({
+        summary,
+        executedAt: new Date(executedAt),
+        userId,
+        id
+      });
+    });
+
+    it('should not call repository then userId is undefined', async () => {
+      const { summary, executedAt, id } = validTaskFixture;
+
+      await expect(taskDomain.update({ summary, executedAt, id })).rejects.toThrowError(
+        `There are one or more invalid fields`
+      );
+      expect(fakeTaskRepository.update).not.toHaveBeenCalled();
+    });
   });
 });
